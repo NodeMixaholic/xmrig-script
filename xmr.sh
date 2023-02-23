ebd323d270c3bb1dcefc567031d78947765efbf7
NodeMixaholic <sam@sllord.info>
2023-02-23 13:50:33 -0600

    Create xmr.sh

---
+++ xmr.sh
@@ -0,0 +1,1 @@
+xmrig -o pool.xmrfast.com:9000 -u 48AKe5DiEH9LPfdPcFQgUhRVSh9NjoWBuDn5JhMsEas3gdzqBgD42Uuj5hzuZ8D1Y7EYV7hq4fVbS5rsMEBg1iLZDwZYqb4 -k --tls -p nmminersfast -o pool.supportxmr.com:443 -u 48AKe5DiEH9LPfdPcFQgUhRVSh9NjoWBuDn5JhMsEas3gdzqBgD42Uuj5hzuZ8D1Y7EYV7hq4fVbS5rsMEBg1iLZDwZYqb4 -k --tls -p nmminers
